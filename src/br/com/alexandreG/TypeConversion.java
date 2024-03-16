4db30ec303a8eaf441eeabbca0acc837f7b9037f
Alexandre Gaia <git config --global user.email>
2024-03-16 14:26:15 -0300

    Initial commit

---
+++ src/br/com/alexandreG/TypeConversion.java
@@ -0,0 +1,16 @@
+/**
+ * @Author: alexandre.gaia
+ */
+
+package br.com.alexandreG;
+
+public class TypeConversion {
+
+    public static void main(String[] args){
+        //conversion types
+        long number_1 = 102323131331313131L;
+        System.out.println(number_1);
+        int number_2 = (int) number_1;
+        System.out.println(number_2);
+    }
+}
